4a580f40dbff0f49b989e4ce0ec2eed0af9c12dc
Sharumallya <165256287+Sharumallya@users.noreply.github.com>
2024-07-18 12:45:35 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,6 +1,3 @@
-# The Snowpark package is required for Python Worksheets. 
-# You can add more packages by selecting them using the Packages control and then importing them.
-
 import snowflake.snowpark as snowpark
 from snowflake.snowpark.functions import col
 
